8994f4547545378f9028bb756c96ca1242b2d939
fritola23 <fritola23@gmail.com>
2018-05-24 17:00:46 -0300

    Modulo Nodemon inserido

--- back/app.js
+++ back/app.js
@@ -5,7 +5,7 @@ app.set('view engide', 'ejs');
 
 //request do index
 app.get('/', function(req, res){
-    res.send("<h1>Ola</h1<");
+    res.send("<h1>Hello!</h1<");
 });
 
 app.listen(3000, function(){
